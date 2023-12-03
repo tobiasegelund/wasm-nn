a6e2e7805cd591eeabde40c02bf67e38a9dc83e5
Tobias Egelund <egelundtobias@gmail.com>
2023-12-03 18:43:32 +0100

    Create burn training loop

--- burn-wasm/src/lib.rs
+++ burn-wasm/src/lib.rs
@@ -1,6 +1,14 @@
 mod model;
 
-use burn::tensor;
+use burn::{
+    nn::loss::CrossEntropyLoss,
+    optim::AdamConfig,
+    optim::GradientsParams,
+    tensor::{
+        backend::{AutodiffBackend, Backend},
+        Int, Tensor,
+    },
+};
 use burn_ndarray::NdArray;
 use model::nn::Model;
 use wasm_bindgen::prelude::*;
@@ -8,11 +16,32 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn inference() -> Vec<f32> {
     let model: Model<NdArray<f32>> = Model::new();
-    let input = tensor::Tensor::<NdArray<f32>, 2>::from_data([[2.]]);
+    let input = Tensor::<NdArray<f32>, 2>::from_data([[2.]]);
     let output = model.forward(input);
 
     output.into_data().value
 }
+
+// #[wasm_bindgen]
+// pub fn train() -> Vec<f32> {
+//     let mut model: Model<NdArray<f32>> = Model::new();
+//     let mut optim = AdamConfig::new().init();
+
+//     let input = Tensor::<NdArray<f32>, 2>::from_floats([[2.]]);
+//     let output = model.forward(input);
+//     let targets: Tensor<NdArray, 1, Int> = Tensor::from_ints([1]);
+
+//     let loss = CrossEntropyLoss::new(None).forward(output.clone(), targets);
+//     let grads = loss.backward();
+//     let grads = GradientsParams::from_grads(grads, &model);
+
+//     model = optim.step(0.01, model, grads);
+
+//     // Output after one epoch
+//     let input = Tensor::<NdArray<f32>, 2>::from_data([[2.]]);
+//     let output = model.forward(input);
+//     output.into_data().value
+// }
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
--- burn-wasm/src/main.rs
+++ burn-wasm/src/main.rs
@@ -1,14 +1,36 @@
 mod model;
 
-use burn::tensor;
+use burn::{
+    backend::{wgpu::WgpuDevice, Autodiff, Wgpu},
+    nn::loss::CrossEntropyLoss,
+    optim::{AdamConfig, GradientsParams, Optimizer},
+    tensor::{backend::AutodiffBackend, Int, Tensor},
+};
 use burn_ndarray::NdArray;
 use model::nn::Model;
 
-fn main() {
-    let model: Model<NdArray<f32>> = Model::new();
-    let input = tensor::Tensor::<NdArray<f32>, 2>::from_data([[2.]]);
-    let output = model.forward(input);
-    let o = output.into_data();
+fn train<B: AutodiffBackend>() {
+    let mut model = Model::<B>::new();
+    let mut optim = AdamConfig::new().init();
 
-    println!("{:?}", o.value);
+    let input: Tensor<B, 2> = Tensor::<B, 2>::from_floats([[2.]]);
+    let output = model.forward(input);
+    println!("{:?}", output.clone().into_data());
+
+    let targets: Tensor<B, 1, Int> = Tensor::from_ints([1]);
+
+    let loss = CrossEntropyLoss::new(None).forward(output.clone(), targets);
+    let grads = loss.backward();
+    let grads = GradientsParams::from_grads(grads, &model);
+
+    model = optim.step(0.01, model, grads);
+
+    // Output after one epoch
+    let input = Tensor::<B, 2>::from_floats([[2.]]);
+    let output = model.forward(input);
+    println!("{:?}", output.clone().into_data());
+}
+
+fn main() {
+    train::<Autodiff<Wgpu>>();
 }
