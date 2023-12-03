1cd8c860f9f625b0c4f173b4735644ac30fd0d5c
Tobias Egelund <egelundtobias@gmail.com>
2023-12-03 15:15:28 +0100

    Burn wasm

--- burn-wasm/src/main.rs
+++ burn-wasm/src/main.rs
@@ -3,18 +3,12 @@ mod model;
 use burn::tensor;
 use burn_ndarray::NdArray;
 use model::nn::Model;
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn inference() -> Vec<f32> {
+fn main() {
     let model: Model<NdArray<f32>> = Model::new();
     let input = tensor::Tensor::<NdArray<f32>, 2>::from_data([[2.]]);
     let output = model.forward(input);
+    let o = output.into_data();
 
-    output.into_data().value
-}
-
-#[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+    println!("{:?}", o.value);
 }
