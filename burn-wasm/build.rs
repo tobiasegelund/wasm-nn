d6b8fcf6a37e6d9d832561414119d441f5c96a65
Tobias Egelund <egelundtobias@gmail.com>
2023-12-03 14:19:34 +0100

    Fix burn build

--- burn-wasm/build.rs
+++ burn-wasm/build.rs
@@ -3,7 +3,7 @@ use burn_import::onnx::ModelGen;
 fn main() {
     // Generate Rust code from the ONNX model file
     ModelGen::new()
-        .input("./src/model/nn.onnx")
-        .out_dir("./src/model/")
+        .input("src/model/nn.onnx")
+        .out_dir("model/")
         .run_from_script();
 }
--- candle-wasm/src/main.rs
+++ candle-wasm/src/main.rs
@@ -1,10 +1,26 @@
 // include!("nn.onnx");
+use candle_core::{Device, Tensor};
 use prost::Message;
+
+use std::collections::HashMap;
 
 const FILE: &'static [u8] = include_bytes!("./nn.onnx");
 
 fn main() {
     let model = candle_onnx::onnx::ModelProto::decode(FILE.as_ref()).unwrap();
 
-    println!("{:?}", model)
+    // let graph = model.graph.as_ref().unwrap();
+
+    let mut inputs = HashMap::new();
+    inputs.insert(
+        "x".to_string(),
+        Tensor::new(&[2f32], &Device::Cpu).unwrap(), // .reshape((1, 1))
+                                                     // .unwrap(),
+    );
+    println!("{:?}", inputs);
+
+    let output = candle_onnx::simple_eval(&model, inputs);
+
+    // println!("{:?}", model);
+    println!("{:?}", output);
 }
