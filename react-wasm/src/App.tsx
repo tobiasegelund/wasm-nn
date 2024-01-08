07215c92957f036e3f686ec7e325ed9919d5e495
Tobias Egelund <egelundtobias@gmail.com>
2024-01-08 17:11:17 +0100

    Clean up

--- react-wasm/src/App.tsx
+++ react-wasm/src/App.tsx
@@ -1,25 +1,9 @@
-import * as ort from 'onnxruntime-web';
-// import init, { train, Model } from "../../pkg/candle_wasm.js";
-import React from "react"
-import Onnx from './components/onnx';
 import './App.css'
+import Onnx from './components/onnx';
 import Candle from './components/candle';
 
 
-type OrtInferenceSession = ort.InferenceSession | null
-
-
 export default function App() {
-    React.useEffect(() => {
-      // init().then(() => {
-      //   const model = new Model();
-      //   console.log(model.train(2., 1));
-      //   console.log(model.train(2., 1));
-      //   console.log(model.train(2., 1));
-      //   // console.log(inference_onnx())
-      // });
-    }, [])
-
   return (
     <div>
       <h1> WASM </h1>
